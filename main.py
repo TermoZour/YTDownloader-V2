cfd6afbd095768543779e9551089ac422be3d745
TermoZour <stefanpruneanu@gmail.com>
2019-04-04 09:23:55 +0300

    added multiple checks
    added check for proper YouTube URL
    added check for file already downloaded
    added exception handling for invalid YouTube URL

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ import os
 from pprint import pprint
 from subprocess import run
 
-from pytube import YouTube
+from pytube import YouTube, exceptions
 
 YT_SEARCH_API_KEY = "asdf"
-YT_URL = "http://www.youtube.com/watch?v="
+YT_URL = "https://www.youtube.com/watch?v="
 DEF_PATH = "downloads"
 START_TEXT = """
 1 - download as mp3
@@ -20,16 +20,30 @@ START_TEXT = """
 """
 
 
-def download_fnc(url, download_type):
-    if download_type is 1:
+def download_fnc(url, download_type, download_path):
+    if download_type is 0:
         # download as mp3
-        query = YouTube(url)
+        try:
+            query = YouTube(url)
+        except exceptions.VideoUnavailable:
+            print("||Couldn't find video||")
+
+            return 0
+        
         stream = query.streams.filter(only_audio=True).order_by("abr").last()
-        # file_name = stream.default_filename()
-        print("Downloading \"{0}\"".format(query.title))
-        stream.download()
+        # check if file is already downloaded
+        if os.path.isfile(os.path.join(download_path, stream.default_filename)):
+            print("||ALREADY DOWNLOADED|| ~~ {0}".format(stream.default_filename))
+
+            return 0
+        else:
+            print("Downloading \"{0}\"".format(query.title))
+            # stream.download(output_path=path)
+            print("||FINISHED|| ~~ {0}".format(query.title))
+
+            return 0
     elif download_type is 2:
-        # download as mp4 video - 1080p
+        # download as mp4 video - highest quality available 
         return 0
 
 
@@ -51,11 +65,16 @@ def url_info(url):
 
 # HERE WE GO - START THE ENGINES
 
-
+# https://www.youtube.com/watch?v=W58FBW93nRc
 while True:
     command = input(START_TEXT)
     if command == "1":
-        download_fnc(input("URL: "), 1)
+        url = input("URL: ")
+        if url.startswith(YT_URL):
+            path = input("PATH: ")
+            download_fnc(url, 0, path)
+        else:
+            print("Wrong URL format!")
         exit(0)
     elif command == "2":
         exit(0)
